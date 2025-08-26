e7d15112547bb757ac48949cbda74da72e87e9c2
Jiayi (Anne) Bian <78373622+bianbian-ai@users.noreply.github.com>
2025-08-26 15:20:58 -0600

    Add files via upload

--- kernelSmoothedPerm.R
+++ kernelSmoothedPerm.R
@@ -3,51 +3,53 @@ library(utilities)
 library(MASS)
 library(moments)
 
-kernelSmoothedPerm <- function(a_standard,a,pvalue_threshold){
+kernelSmoothedPerm <- function(X_standard, X, pvalue_threshold){
   ### 0. P-value estimation for naive permutation
-  pvalue_naive = sum(a >= a_standard)/length(a)
+  pvalue_naive = sum(X >= X_standard)/length(X)
   
   ### 1. check kurtosis of permuted test statistics
-  if(abs(kurtosis(a)-3) <= 0.01){
-    lambda_a_optinal = "none"
-    a_trans = a
-    a_standard_trans = a_standard  ## go to 4
+  if(abs(kurtosis(X)-3) <= 0.01){
+    lambda_X_optinal = "none"
+    X_trans = X
+    X_standard_trans = X_standard  ## go to 4
   } else {
     ### 2. Box-Cox transformation: data must be positive (no negative values) and continuous
     ###    If there is negative value, then data should be normalized.
-    if(sum(a <= 0) > 0){
-      a = (a-min(a))/(max(a)-min(a))
-      a[which(a == 0)] = 1e-05
+    if(sum(X <= 0) > 0){
+      X = (X-min(X))/(max(X)-min(X))
+      X[which(X == 0)] = 1e-05
     }
-    cox_a = boxcox(lm(a ~ 1),plotit = F)
-    lambda_a = cox_a$x[which.max(cox_a$y)]
+    cox_X = boxcox(lm(X ~ 1),plotit = F)
+    lambda_X = cox_X$x[which.max(cox_X$y)]
     
-    if(lambda_a == 0){
-      lambda_a_optinal = 0
-      a_trans = log(a,base = exp(1))
-      a_standard_trans = log(a_standard,base = exp(1))  ## go to 4
+    ### 3. find the optimal power and transform the data
+    ## 3.1 if optimal power = 0, then perform log transformation
+    if(lambda_X == 0){
+      lambda_X_optinal = 0
+      X_trans = log(X,base = exp(1))
+      X_standard_trans = log(X_standard,base = exp(1))  ## go to 4
     } else {
-      ### 3. find the optimal kurtosis-driven power and transform the data
-      lambda_a_test = lambda_a-seq(-0.2,0.2,0.01)
-      lambda_a_test = lambda_a_test[lambda_a_test != 0]
-      kurtosis_a_test = numeric(0)
-      for (i in 1:length(lambda_a_test)) {
-        a_trans_test = (a^lambda_a_test[i]-1)/lambda_a_test[i]
-        kurtosis_a_test[i] = kurtosis(a_trans_test)
+      ## 3.2 find the optimal kurtosis-driven power and transform the data
+      lambda_X_test = lambda_X-seq(-0.2,0.2,0.01)
+      lambda_X_test = lambda_X_test[lambda_X_test != 0]
+      kurtosis_X_test = numeric(0)
+      for (i in 1:length(lambda_X_test)) {
+        X_trans_test = (X^lambda_X_test[i]-1)/lambda_X_test[i]
+        kurtosis_X_test[i] = kurtosis(X_trans_test)
       }
-      lambda_a_optinal = lambda_a_test[which.min(abs(kurtosis_a_test-3))]
-      a_trans = (a^lambda_a_optinal-1)/lambda_a_optinal
-      a_standard_trans = (a_standard^lambda_a_optinal-1)/lambda_a_optinal ## go to 4
+      lambda_X_optinal = lambda_X_test[which.min(abs(kurtosis_X_test-3))]
+      X_trans = (X^lambda_X_optinal-1)/lambda_X_optinal
+      X_standard_trans = (X_standard^lambda_X_optinal-1)/lambda_X_optinal ## go to 4
     }
   }
   
-  ### 4. apply KDE based on P-value accuracy threshold
+  ### 4. apply KDE based on P-value accuracy threshold, need to change 50???
   if(pvalue_threshold == 1e-07){
     pvalue_test = numeric(0)
     for (j in 1:50){
-      a_trans_test = sample(a_trans,1e+07,replace = F)
-      MY_KDE = KDE(a_trans_test,bandwidth = 5*bw.nrd0(a_trans_test),to.environment = T)
-      pvalue_test[j] = pkde(a_standard_trans,lower.tail = F)
+      X_trans_test = sample(X_trans,1e+07,replace = F)
+      MY_KDE = KDE(X_trans_test,bandwidth = 5*bw.nrd0(X_trans_test),to.environment = T)
+      pvalue_test[j] = pkde(X_standard_trans,lower.tail = F)
     }
     pvalue_kernel = median(pvalue_test)
   }  
@@ -55,20 +57,15 @@ kernelSmoothedPerm <- function(a_standard,a,pvalue_threshold){
   if(pvalue_threshold == 1e-08){
     pvalue_test = numeric(0)
     for (j in 1:50){
-      a_trans_test = sample(a_trans,1e+08,replace = F)
-      MY_KDE = KDE(a_trans_test,bandwidth = 7*bw.nrd0(a_trans_test),to.environment = T)
-      pvalue_test[j] = pkde(a_standard_trans,lower.tail = F)
+      X_trans_test = sample(X_trans,1e+08,replace = F)
+      MY_KDE = KDE(X_trans_test,bandwidth = 7*bw.nrd0(X_trans_test),to.environment = T)
+      pvalue_test[j] = pkde(X_standard_trans,lower.tail = F)
     }
     pvalue_kernel = median(pvalue_test)
   }
   
-  newlist = list(optimal_lambda = lambda_a_optinal, pvalue_naive = pvalue_naive, pvalue_kernel = pvalue_kernel)
+  newlist = list(optimal_lambda = lambda_X_optinal, pvalue_naive = pvalue_naive, pvalue_kernel = pvalue_kernel)
   return(newlist)
 }
-
-
-
-
-
 
 
